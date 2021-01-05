55ddd8d476a1b4bef2071ed69e8410789be9e285
Nariden <nariden90@gmail.com>
2021-01-05 15:33:14 -0600

    This is my first commit

---
+++ src/test/java/com/inetbanking/utilities/ReadConfig.java
@@ -0,0 +1,65 @@
+package com.inetbanking.utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ReadConfig {
+
+	Properties pro;
+	
+	public ReadConfig()//Create a Constructor
+	{
+		File src = new File("./Configuration/config.properties");//specify the path of prop. file
+
+		try {
+			FileInputStream fis = new FileInputStream(src);//Read the file
+			pro = new Properties();
+			pro.load(fis);//Load the property file
+		} catch (Exception e) {
+			System.out.println("Exception is " + e.getMessage());
+		}
+	}
+	
+	public String getApplicationURL()
+	{
+		String url=pro.getProperty("baseURL");
+		return url;
+	}
+	
+	public String getUsername()
+	{
+	String username=pro.getProperty("username");
+	return username;
+	}
+	
+	public String getPassword()
+	{
+	String password=pro.getProperty("password");
+	return password;
+	}
+	
+	public String getChromePath()
+	{
+	String chromepath=pro.getProperty("chromepath");
+	return chromepath;
+	}
+	
+	public String getIEPath()
+	{
+	String iepath=pro.getProperty("iepath");
+	return iepath;
+	}
+	
+	public String getFirefoxPath()
+	{
+	String firefoxpath=pro.getProperty("firefoxpath");
+	return firefoxpath;
+	}
+
+	
+}
+
+
+
+
